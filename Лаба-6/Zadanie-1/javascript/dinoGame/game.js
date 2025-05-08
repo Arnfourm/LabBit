f8e9f99058a4317db95a3e67c5e2a2a0ec0884e6
Luverk1d <arnfourm@gmail.com>
2025-05-08 21:36:24 +0300

    lab 6 1 task v1.0

--- Лаба-6/Zadanie-1/javascript/dinoGame/game.js
+++ Лаба-6/Zadanie-1/javascript/dinoGame/game.js
@@ -22,9 +22,9 @@ const cactus = document.getElementById("cactus");
 let isAlive = setInterval(function() {
     let dinoTop = parseInt(window.getComputedStyle(dino).getPropertyValue("top"));
 
-    let cactusLeft = parseInt(window.getComputedStyle(cactus).getPropertyPriority("left"));
+    let cactusLeft = parseInt(window.getComputedStyle(cactus).getPropertyValue("left"));
 
-    if (cactusLeft < 50 && cactusLeft > 0 && dinoTop >= 140){
+    if (cactusLeft < 50 && cactusLeft > 0 && dinoTop > 290){
         alert("=(");
     };
 }, 10);
